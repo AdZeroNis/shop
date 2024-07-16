2c5a0e192e7edd410c5ea6f9833f0a254cd03737
AdZeroNis <shghyghkrimi2923@gmail.com>
2024-07-16 15:24:30 +0330

    start

---
+++ resources/views/Admin/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+ @include("Admin.layouts.head-tag")
+ <body>
+    <div id="load_screen"> <div class="loader"> <div class="loader-content">
+        <div class="spinner-grow align-self-center"></div>
+    </div></div></div>
+    @include("Admin.layouts.Navbar-part")
+    @include("Admin.layouts.Navbar-part-two")
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+        @include("Admin.layouts.sidebar-part")
+        <div id="content" class="main-content">
+            @include("Admin.layouts.footer-tag")
+        </div>
+        @include("Admin.layouts.js")
+ </body>
+
+
+
+
+</html>
